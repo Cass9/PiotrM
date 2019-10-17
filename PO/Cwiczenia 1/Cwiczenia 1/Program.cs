4019530bfdf3fcff350090b984f4b7fd34d8c9e8
Cass9 <46461842+Cass9@users.noreply.github.com>
2019-10-17 21:23:34 +0200

    .

---
+++ PO/Cwiczenia 1/Cwiczenia 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 1");
+
+            int[] tab = new int[] { 1, 2, 3, 4, 5, 6 };
+            for (int i = 0; i < tab.Length - 1; i++)
+            {
+                Console.Write(tab[i] + ",");
+            }
+            Console.Write(tab[tab.Length - 1]);
+            Console.WriteLine();
+
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.WriteLine(tab[i]);
+            }
+
+            for (int i = tab.Length-1; i >= 0; i--)
+            {
+                Console.Write(tab[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
