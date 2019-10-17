fac0f24e5145cf62aed010e3cb3eb8dc711607dd
Cass9 <46461842+Cass9@users.noreply.github.com>
2019-10-17 22:02:01 +0200

    .

---
+++ PO/Cwiczenia 2/Cwiczenia 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia_2
+{
+    class Program
+    {
+        public class Car
+        {
+            private string marka;
+            private int rok;
+            private string model;
+            private int iloscDrzwi;
+            private string pojemnoscSilnika;
+            public double srednieSpalanie;
+
+            public Car(string n, int z)
+            {
+                marka = n;
+                rok = z;
+            }
+            public void PokazCar()
+            {
+                Console.WriteLine(marka + " " + rok);
+            }
+            private double ObliczSpalanie(double srednieSpalanie, double dlugoscTrasy)
+            {
+                double spalanie;
+                return spalanie = srednieSpalanie * dlugoscTrasy;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string carName;
+            carName = "Moj samochod";
+
+            Car car1 = new Car("Skoda", 2004);
+            Car car2 = new Car("Opel", 1999);
+            /*
+            car1 = car2;
+            */
+            car1.PokazCar();
+            car2.PokazCar();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
