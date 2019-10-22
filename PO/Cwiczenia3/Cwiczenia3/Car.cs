1fb27dcee9f44e56bb1648fbf2267f4d5203f266
Cass9 <46461842+Cass9@users.noreply.github.com>
2019-10-22 21:53:12 +0200

    .

---
+++ PO/Cwiczenia3/Cwiczenia3/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cwiczenia3
+{
+    class Car
+    {
+        private double pojemnoscSilnika;
+        private string marka;
+        static int iloscKol;
+
+        private Car()
+        {
+
+        }
+
+        static Car()
+        {
+            iloscKol = 4;
+        }
+
+        private Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+
+        public static Car Create()
+        {
+            Car c = new Car();
+            return c;
+        }
+
+        ~Car()
+        {
+            Console.WriteLine("Zwalniam pamięć");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PO/Cwiczenia3/Cwiczenia3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cwiczenia3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car t = Car.Create();
+        }
+    }
+}
