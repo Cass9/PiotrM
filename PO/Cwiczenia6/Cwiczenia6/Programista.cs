22435cc5e31dbd379527044a3dd96c5033108425
Cass9 <46461842+Cass9@users.noreply.github.com>
2019-11-09 16:52:22 +0100

    .

---
+++ PO/Cwiczenia6/Cwiczenia6/Programista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia6
+{
+    class Programista : Pracownik
+    {
+        public override void Pracuj()
+        {
+            base.Pracuj();
+            Console.WriteLine("Obowiazki programisty");
+        }
+    }
+}
